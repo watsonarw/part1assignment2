8644e99bdaf7eda99bdbf7cbab9a7e785cf151b6
Andrew Watson <watson.arw@gmail.com>
2015-04-15 02:24:22 +1000

    prime factors

--- src/Introductory.java
+++ src/Introductory.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Created by watsonarw on 14/04/15.
  */
@@ -10,5 +18,46 @@ public class Introductory {
         Diamond.isoscelesTriangle(3);
         Diamond.diamond(3);
         Diamond.diamondWithName(3);
+        fizzBuzz();
+        System.out.println(generate(30));
     }
-} 
+
+    private static void fizzBuzz() {
+        StringBuilder sb = new StringBuilder();
+        IntStream.rangeClosed(1, 100)
+                .forEachOrdered(i -> {
+                    boolean flag = false;
+                    if (i % 3 == 0) {
+                        sb.append("Fizz");
+                        flag = true;
+                    }
+                    if (i % 5 == 0) {
+                        sb.append("Buzz");
+                        flag = true;
+                    }
+                    if (!flag) {
+                        sb.append(i);
+                    }
+                    sb.append("\n");
+                });
+        System.out.println(sb.toString());
+    }
+
+    private static List<Integer> generate(int n) {
+        List<Integer> primeFactors = IntStream.rangeClosed(2, n)
+                 .parallel()
+                 .filter(i -> n % i == 0)
+                 .filter(i -> isPrime(i))
+                 .sorted()
+                 .boxed()
+                 .collect(Collectors.toList());
+        return primeFactors;
+    }
+
+    private static boolean isPrime(int n) {
+        return IntStream.range(2,n)
+                 .parallel()
+                 .filter(i -> n % i == 0)
+                 .toArray().length == 0;
+    }
+}
